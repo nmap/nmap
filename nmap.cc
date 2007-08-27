8d0b39dadb71668b97f933341761b79f824c5cb3
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2007-08-27 03:18:56 +0000

    capitalize Lua

--- nmap.cc
+++ nmap.cc
@@ -256,7 +256,7 @@ printf("%s %s ( %s )\n"
 #ifndef NOLUA
        "SCRIPT SCAN:\n"
        "  -sC: equivalent to --script=safe,intrusive\n"
-       "  --script=<lua scripts>: <lua scripts> is a comma separated list of \n"
+       "  --script=<Lua scripts>: <Lua scripts> is a comma separated list of \n"
 	   "           directories, script-files or script-categories\n"
 	   "  --script-args=<n1=v1,[n2=v2,...]>: provide arguments to scripts\n"
        "  --script-trace: Show all data sent and received\n"
