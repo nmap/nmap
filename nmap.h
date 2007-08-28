a84a7d14c886df9f9950dba55d4d40c05316bc61
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2007-08-28 09:07:12 +0000

    change version number to 4.22SOC6

--- nmap.h
+++ nmap.h
@@ -253,8 +253,8 @@ void *realloc();
 #ifndef NMAP_VERSION
 /* Edit this definition only within the quotes, because it is read from this
    file by the makefiles. */
-#define NMAP_VERSION "4.22SOC5"
-#define NMAP_NUM_VERSION "4.22.0.5"
+#define NMAP_VERSION "4.22SOC6"
+#define NMAP_NUM_VERSION "4.22.0.6"
 #endif
 
 /* User configurable #defines: */
