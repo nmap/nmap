0a2764f1980293c105bfabd2d4f07c07164be6a9
david <david@e0a8ed71-7df4-0310-8962-fdc924857419>
2007-10-12 03:46:46 +0000

    Remove massping.
    2 files changed, 1301 deletions(-)
    
    Would that all patches could be like that.

--- targets.h
+++ targets.h
@@ -126,10 +126,6 @@
 #include "nmap.h"
 #include "global_structures.h"
 
-/* If defined, use the new massping that uses ultra_scan instead of the old
-   standalone function. */
-#define NEW_MASSPING
-
 class HostGroupState;
 
 /**************************STRUCTURES******************************/
@@ -160,9 +156,6 @@ struct tcpqueryinfo {
 
 struct pingtech {
   unsigned int rawicmpscan: 1,
-#ifndef NEW_MASSPING
-    icmpscan: 1,
-#endif
     connecttcpscan: 1,
     rawtcpscan: 1,
     rawudpscan: 1;
