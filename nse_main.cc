c30adcd5bfdc88af90a9c4ddb012e1dd6feb3c49
stoiko <stoiko@e0a8ed71-7df4-0310-8962-fdc924857419>
2007-12-08 17:23:38 +0000

    perform lua-garbage collection everytime a script finishes excecution (should solve the endless loops of NSE)

--- nse_main.cc
+++ nse_main.cc
@@ -299,6 +299,7 @@ int process_mainloop(lua_State* l) {
 			}
 
 			SCRIPT_ENGINE_TRY(process_finalize(l, current.registry_idx));
+			SCRIPT_ENGINE_TRY(lua_gc(l, LUA_GCCOLLECT,NULL));
 		} else {
 			// this script returned because of an error
 			// print the failing reason if the verbose level is high enough	
