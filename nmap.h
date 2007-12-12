0012ee3deead26e646eaef45e4fee1901159f8dc
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2007-12-12 10:38:52 +0000

    Update CHANGELOG and version numbers for what is hoped to become 4.50

--- nmap.h
+++ nmap.h
@@ -253,8 +253,8 @@ void *realloc();
 #ifndef NMAP_VERSION
 /* Edit this definition only within the quotes, because it is read from this
    file by the makefiles. */
-#define NMAP_VERSION "4.49RC7"
-#define NMAP_NUM_VERSION "4.49.0.7"
+#define NMAP_VERSION "4.50"
+#define NMAP_NUM_VERSION "4.50.0.0"
 #endif
 
 /* User configurable #defines: */
