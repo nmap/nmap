74a790baabf777f775e37ada94df6437cfed8f61
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2007-12-22 02:17:42 +0000

    fix version number

--- nmap.h
+++ nmap.h
@@ -253,7 +253,7 @@ void *realloc();
 #ifndef NMAP_VERSION
 /* Edit this definition only within the quotes, because it is read from this
    file by the makefiles. */
-#define NMAP_VERSION "4.51"
+#define NMAP_VERSION "4.51BETA"
 #define NMAP_NUM_VERSION "4.51.0.0"
 #endif
 
