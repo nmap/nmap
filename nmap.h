478c06b0abdcdc0f0c46bb3adc047a78f1b8b8e6
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2009-01-30 05:15:24 +0000

    prepare for 4.85BETA2 release

--- nmap.h
+++ nmap.h
@@ -256,8 +256,8 @@ void *realloc();
 #ifndef NMAP_VERSION
 /* Edit this definition only within the quotes, because it is read from this
    file by the makefiles. */
-#define NMAP_VERSION "4.85BETA1"
-#define NMAP_NUM_VERSION "4.85.0.1"
+#define NMAP_VERSION "4.85BETA2"
+#define NMAP_NUM_VERSION "4.85.0.2"
 #endif
 
 /* User configurable #defines: */
