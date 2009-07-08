4e9d7c17a54504d945b67237ddf25e3a4966b8dc
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2009-07-08 22:32:49 +0000

    update Nmap version number to 4.90RC1, rebuild generated files, except for man pages which are problematic at the moment

--- nmap.h
+++ nmap.h
@@ -252,8 +252,8 @@ void *realloc();
 #ifndef NMAP_VERSION
 /* Edit this definition only within the quotes, because it is read from this
    file by the makefiles. */
-#define NMAP_VERSION "4.85BETA10"
-#define NMAP_NUM_VERSION "4.85.0.10"
+#define NMAP_VERSION "4.90RC2"
+#define NMAP_NUM_VERSION "4.90.0.2"
 #endif
 
 /* User configurable #defines: */
