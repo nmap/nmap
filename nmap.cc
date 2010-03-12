5318dd3c58fc634a7d87d86c9ff2d81ca9884a8a
david <david@e0a8ed71-7df4-0310-8962-fdc924857419>
2010-03-12 21:20:05 +0000

    Only write host elements for down hosts to XML in verbose mode. This makes it consistent with other output formats.

--- nmap.cc
+++ nmap.cc
@@ -1766,9 +1766,11 @@ int nmap_main(int argc, char *argv[]) {
       /* I used to check that !currenths->weird_responses, but in some
 	 rare cases, such IPs CAN be port successfully scanned and even connected to */
       if (!(currenths->flags & HOST_UP)) {
-	log_write(LOG_XML, "<host>");
-	write_host_header(currenths);
-	log_write(LOG_XML, "</host>\n");
+	if (o.verbose) {
+	  log_write(LOG_XML, "<host>");
+	  write_host_header(currenths);
+	  log_write(LOG_XML, "</host>\n");
+	}
 	delete currenths;
 	o.numhosts_scanned++;
 	continue;
