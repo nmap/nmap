537a65d7d55608319bd0f7a606d63658041eb858
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2010-03-16 03:19:47 +0000

    Just update an out-of-date (but was still working due to redirection) URL

--- nmap_tty.cc
+++ nmap_tty.cc
@@ -270,7 +270,7 @@ bool keyWasPressed()
 		"d/D             Increase/decrease debugging\n"
 		"p/P             Enable/disable packet tracing\n"
 		"anything else   Print status\n"
-                "More help: http://nmap.org/man/man-runtime-interaction.html\n");
+                "More help: http://nmap.org/book/man-runtime-interaction.html\n");
     } else {
        printStatusMessage();
        return true;
