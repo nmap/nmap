050548f5624ce2c396d6ce99e615b360b10e6d5e
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2010-11-20 08:36:49 +0000

    Change version number from 5.36TEST1 to TEST2

--- nmap.h
+++ nmap.h
@@ -252,8 +252,8 @@ void *realloc();
 #ifndef NMAP_VERSION
 /* Edit this definition only within the quotes, because it is read from this
    file by the makefiles. */
-#define NMAP_VERSION "5.36TEST1"
-#define NMAP_NUM_VERSION "5.36.0.1"
+#define NMAP_VERSION "5.36TEST2"
+#define NMAP_NUM_VERSION "5.36.0.2"
 #endif
 
 /* User configurable #defines: */
