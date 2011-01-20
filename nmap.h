f037a54cd77ed3be0048b6d0b54445fee44d167a
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2011-01-20 10:29:18 +0000

    Update Nmap version number to 5.36TEST4 in prep for the next release in a day or two and regenerate relevant files

--- nmap.h
+++ nmap.h
@@ -252,8 +252,8 @@ void *realloc();
 #ifndef NMAP_VERSION
 /* Edit this definition only within the quotes, because it is read from this
    file by the makefiles. */
-#define NMAP_VERSION "5.36TEST3"
-#define NMAP_NUM_VERSION "5.36.0.3"
+#define NMAP_VERSION "5.36TEST4"
+#define NMAP_NUM_VERSION "5.36.0.4"
 #endif
 
 /* User configurable #defines: */
