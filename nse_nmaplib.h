6f57ba85ea041adff9ccf59d5499a4a2ea05f15e
djalal <djalal@e0a8ed71-7df4-0310-8962-fdc924857419>
2011-02-10 08:45:50 +0000

    Remove get_target() and get_port() function declaration from the nse_nmaplib.h since they are already declared in the nse_utility.h

--- nse_nmaplib.h
+++ nse_nmaplib.h
@@ -11,8 +11,6 @@ int luaopen_nmap(lua_State* l);
 int luaopen_stdnse_c (lua_State *L);
 void set_hostinfo(lua_State* l, Target* currenths);
 void set_portinfo(lua_State* l, const Target *target, const Port* port);
-Target *get_target (lua_State *L, int index);
-Port *get_port (lua_State *L, Target *target, Port *port, int index);
 
 #endif
 
