9d2463b9cdcf75e1a40b2a7b6fb3b23c06a050af
batrick <batrick@e0a8ed71-7df4-0310-8962-fdc924857419>
2011-04-05 19:34:34 +0000

    comment clarifying -1 case

--- nse_nsock.cc
+++ nse_nsock.cc
@@ -757,7 +757,7 @@ static int l_set_timeout (lua_State *L)
 {
   nse_nsock_udata *nu = check_nsock_udata(L, 1, 0);
   nu->timeout = luaL_checkint(L, 2);
-  if ((int) nu->timeout < -1)
+  if ((int) nu->timeout < -1) /* -1 is no timeout */
     return luaL_error(L, "Negative timeout: %d", nu->timeout);
   return success(L);
 }
