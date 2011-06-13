540ee6261156a45194a3c873ce1acf81c01a94da
luis <luis@e0a8ed71-7df4-0310-8962-fdc924857419>
2011-06-13 10:04:20 +0000

    Revert r23288 as it seems to crash Nmap. More info: http://seclists.org/nmap-dev/2011/q2/926

--- osscan2.cc
+++ osscan2.cc
@@ -1344,6 +1344,14 @@ void HostOsScan::sendT1_7Probe(HostOsScanStats *hss, int probeNo) {
   int port_base = tcpPortBase + NUM_SEQ_SAMPLES + 7;
 
   switch(probeNo) {
+  case 0: /* T1 */
+    if(hss->openTCPPort == -1) return;
+    send_tcp_probe(hss, o.ttl, false, NULL, 0,
+                   port_base, hss->openTCPPort,
+                   tcpSeqBase, tcpAck,
+                   0, TH_SYN, prbWindowSz[0], 0,
+                   prbOpts[0].val, prbOpts[0].len, NULL, 0);
+    break;
   case 1: /* T2 */
     if(hss->openTCPPort == -1) return;
     send_tcp_probe(hss, o.ttl, true, NULL, 0,
@@ -1391,11 +1399,6 @@ void HostOsScan::sendT1_7Probe(HostOsScanStats *hss, int probeNo) {
                    tcpSeqBase, tcpAck,
                    0, TH_FIN|TH_PUSH|TH_URG, prbWindowSz[12], 0,
                    prbOpts[12].val, prbOpts[12].len, NULL, 0);
-    break;
-  default:
-    fatal("Wrong probe number (%d) passed to %s()", probeNo, __func__);
-    break;
-  
   }
 }
 
