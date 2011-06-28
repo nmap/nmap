832d1e91fac50839cd654075022a6d5e762a969c
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2011-06-28 22:19:31 +0000

    Update version number for next release (5.59BETA1)

--- nmap.h
+++ nmap.h
@@ -252,8 +252,8 @@ void *realloc();
 #ifndef NMAP_VERSION
 /* Edit this definition only within the quotes, because it is read from this
    file by the makefiles. */
-#define NMAP_VERSION "5.52.IPv6.Beta2"
-#define NMAP_NUM_VERSION "5.52.0.2"
+#define NMAP_VERSION "5.59BETA1"
+#define NMAP_NUM_VERSION "5.59.0.1"
 #endif
 
 /* User configurable #defines: */
