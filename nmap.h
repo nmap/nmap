5b516e982db362cdfb421108f047f8721f562429
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2011-08-25 06:47:10 +0000

    Update svn version number to 5.59BETA3.  There isn't an imminent release or anything, but I think bumping it will reduce confusion. Plus, I accidentally checked in a modified version number to some of the externals (when meaning to just modify a branch), so this will make the version consistent

--- nmap.h
+++ nmap.h
@@ -252,8 +252,8 @@ void *realloc();
 #ifndef NMAP_VERSION
 /* Edit this definition only within the quotes, because it is read from this
    file by the makefiles. */
-#define NMAP_VERSION "5.59BETA1"
-#define NMAP_NUM_VERSION "5.59.0.1"
+#define NMAP_VERSION "5.59BETA3"
+#define NMAP_NUM_VERSION "5.59.0.3"
 #endif
 
 /* User configurable #defines: */
