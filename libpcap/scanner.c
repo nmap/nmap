364e6f64d8f460bfada7c42d7da49fca75e05030
david <david@e0a8ed71-7df4-0310-8962-fdc924857419>
2011-09-21 17:41:21 +0000

    Regenerate scanner.c with flex 2.5.35.
    ./runlex.sh flex -Ppcap_ -oscanner.c scanner.l

--- libpcap/scanner.c
+++ libpcap/scanner.c
@@ -73,7 +73,6 @@ typedef int flex_int32_t;
 typedef unsigned char flex_uint8_t; 
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
-#endif /* ! C99 */
 
 /* Limits of integral types. */
 #ifndef INT8_MIN
@@ -103,6 +102,8 @@ typedef unsigned int flex_uint32_t;
 #ifndef UINT32_MAX
 #define UINT32_MAX             (4294967295U)
 #endif
+
+#endif /* ! C99 */
 
 #endif /* ! FLEXINT_H */
 
@@ -160,7 +161,15 @@ typedef unsigned int flex_uint32_t;
 
 /* Size of default input buffer. */
 #ifndef YY_BUF_SIZE
+#ifdef __ia64__
+/* On IA-64, the buffer size is 16k, not 8k.
+ * Moreover, YY_BUF_SIZE is 2*YY_READ_BUF_SIZE in the general case.
+ * Ditto for the __ia64__ case accordingly.
+ */
+#define YY_BUF_SIZE 32768
+#else
 #define YY_BUF_SIZE 16384
+#endif /* __ia64__ */
 #endif
 
 /* The state buf must be large enough to hold one state per character in the main buffer.
@@ -2772,7 +2781,7 @@ static const char *in_buffer;
 #define yylval pcap_lval
 extern YYSTYPE yylval;
 
-#line 2776 "scanner.c"
+#line 2785 "scanner.c"
 
 #define INITIAL 0
 
@@ -2835,7 +2844,12 @@ static int input (void );
 
 /* Amount of stuff to slurp up with each read. */
 #ifndef YY_READ_BUF_SIZE
+#ifdef __ia64__
+/* On IA-64, the buffer size is 16k, not 8k */
+#define YY_READ_BUF_SIZE 16384
+#else
 #define YY_READ_BUF_SIZE 8192
+#endif /* __ia64__ */
 #endif
 
 /* Copy whatever the last rule matched to the standard output. */
@@ -2843,7 +2857,7 @@ static int input (void );
 /* This used to be an fputs(), but since the string might contain NUL's,
  * we now use fwrite().
  */
-#define ECHO fwrite( pcap_text, pcap_leng, 1, pcap_out )
+#define ECHO do { if (fwrite( pcap_text, pcap_leng, 1, pcap_out )) {} } while (0)
 #endif
 
 /* Gets input and stuffs it into "buf".  number of characters read, or YY_NULL,
@@ -2854,7 +2868,7 @@ static int input (void );
 	if ( YY_CURRENT_BUFFER_LVALUE->yy_is_interactive ) \
 		{ \
 		int c = '*'; \
-		int n; \
+		size_t n; \
 		for ( n = 0; n < max_size && \
 			     (c = getc( pcap_in )) != EOF && c != '\n'; ++n ) \
 			buf[n] = (char) c; \
@@ -2938,7 +2952,7 @@ YY_DECL
     
 #line 188 "scanner.l"
 
-#line 2942 "scanner.c"
+#line 2956 "scanner.c"
 
 	if ( !(yy_init) )
 		{
@@ -3780,7 +3794,7 @@ YY_RULE_SETUP
 #line 389 "scanner.l"
 ECHO;
 	YY_BREAK
-#line 3784 "scanner.c"
+#line 3798 "scanner.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -4501,8 +4515,8 @@ YY_BUFFER_STATE pcap__scan_string (yyconst char * yystr )
 
 /** Setup the input buffer state to scan the given bytes. The next call to pcap_lex() will
  * scan from a @e copy of @a bytes.
- * @param bytes the byte buffer to scan
- * @param len the number of bytes in the buffer pointed to by @a bytes.
+ * @param yybytes the byte buffer to scan
+ * @param _yybytes_len the number of bytes in the buffer pointed to by @a bytes.
  * 
  * @return the newly allocated buffer state object.
  */
