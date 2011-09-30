nmap/nmap

commit 1563511392a894c6be3867ea7f56372925e31faa

Originally, adding the script to the array of scripts would handle the case where the Script.new failed:
    chosen_scripts[#chosen_scripts+1] = Script.new(path);

if Script.new returns nil, then the operation is a no-op (as intended). So,
just add the script to the array. Script.new now accepts a selected_by_name
argument so we don't set it manually.


git-svn-id: https://svn.nmap.org/nmap@23911 e0a8ed71-7df4-0310-8962-fdc924857419

nse_main.lua
@@ -380,7 +380,7 @@ do
   --   filename  The filename (path) of the script to load.
   -- Returns:
   --   script  The script (class) created.
-  function Script.new (filename)
+  function Script.new (filename, selected_by_name)
     assert(type(filename) == "string", "string expected");
     if not find(filename, "%.nse$") then
       log_error(
@@ -457,7 +457,7 @@ do
       license = rawget(env, "license"),
       dependencies = rawget(env, "dependencies"),
       threads = {},
-      selected_by_name = false,
+      selected_by_name = not not selected_by_name,
     };
     return setmetatable(script, {__index = Script, __metatable = Script});
   end
@@ -567,8 +567,7 @@ local function get_chosen_scripts (rules)
         local t, path = cnse.fetchscript(filename);
         if t == "file" then
           if not files_loaded[path] then
-            script = Script.new(path);
-            chosen_scripts[#chosen_scripts+1] = script;
+            chosen_scripts[#chosen_scripts+1] = Script.new(path);
             files_loaded[path] = true;
             -- do not break so other rules can be marked as used
           end
@@ -599,10 +598,9 @@ local function get_chosen_scripts (rules)
       if t == nil then
         error("'"..rule.."' did not match a category, filename, or directory");
       elseif t == "file" and not files_loaded[path] then
-        local script = Script.new(path);
-        script.selected_by_name = true;
+        local script = Script.new(path, true);
         chosen_scripts[#chosen_scripts+1] = script;
-        print_debug(2, "Script %s was selected by name.", script.filename);
+        print_debug(2, "Script %s was selected by name.", path);
         files_loaded[path] = true;
       elseif t == "directory" then
         for f in cnse.dir(path) do

commit 110b0d35b0535bc659900c4a7e8f095cf0ff437a

Fixed a bug I introduced in r23911 that caused some "rules" to not mark a script as selected by name. Removed some dead code.

nse_main.lua
@@ -395,6 +395,9 @@ do
           filename);
     end
     local basename = match(filename, "([^/\\]+)$") or filename;
+    if selected_by_name then
+      print_debug(2, "Script %s was selected by name.", basename);
+    end
     local short_basename = match(filename, "([^/\\]+)%.nse$") or
         match(filename, "([^/\\]+)%.[^.]*$") or filename;
     local file_closure = assert(loadfile(filename));
@@ -570,14 +573,13 @@ local function get_chosen_scripts (rules)
     end
     local env = {m = m};
 
-    local script;
     for globalized_rule, rule_table in pairs(entry_rules) do
       if setfenv(rule_table.compiled_rule, env)() then -- run the compiled rule
         used_rules[rule_table.original_rule] = true;
         local t, path = cnse.fetchscript(filename);
         if t == "file" then
           if not files_loaded[path] then
-            chosen_scripts[#chosen_scripts+1] = Script.new(path);
+            chosen_scripts[#chosen_scripts+1] = Script.new(path, selected_by_name);
             files_loaded[path] = true;
             -- do not break so other rules can be marked as used
           end
@@ -585,12 +587,6 @@ local function get_chosen_scripts (rules)
           log_error("Warning: Could not load '%s': %s", filename, path);
           break;
         end
-      end
-    end
-    if script then
-      script.selected_by_name = selected_by_name;
-      if script.selected_by_name then
-        print_debug(2, "Script %s was selected by name.", script.basename);
       end
     end
   end
@@ -610,7 +606,6 @@ local function get_chosen_scripts (rules)
       elseif t == "file" and not files_loaded[path] then
         local script = Script.new(path, true);
         chosen_scripts[#chosen_scripts+1] = script;
-        print_debug(2, "Script %s was selected by name.", path);
         files_loaded[path] = true;
       elseif t == "directory" then
         for f in cnse.dir(path) do