11f76fc18918b9b8e43847910ddff6d46ecc2374
david <david@e0a8ed71-7df4-0310-8962-fdc924857419>
2011-12-19 23:13:02 +0000

    Add win_config.h, missed earlier.

---
+++ nmap-update/win_config.h
@@ -0,0 +1,64 @@
+/* win_config.h. */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+/* #undef HAVE_INTTYPES_H */
+
+/* Define to 1 if you have the `apr-1' library (-lapr-1). */
+#define HAVE_LIBAPR_1 1
+
+/* Define to 1 if you have the `svn_client-1' library (-lsvn_client-1). */
+#define HAVE_LIBSVN_CLIENT_1 1
+
+/* Define to 1 if you have the `svn_subr-1' library (-lsvn_subr-1). */
+#define HAVE_LIBSVN_SUBR_1 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <subversion-1/svn_client.h> header file. */
+/* #undef HAVE_SUBVERSION_1_SVN_CLIENT_H */
+
+/* Define to 1 if you have the <svn_client.h> header file. */
+#define HAVE_SVN_CLIENT_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+/* #undef HAVE_UNISTD_H */
+
+/* Define to the address where bug reports for this package should be sent. */
+#undef PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#undef PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#undef PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#undef PACKAGE_TARNAME
+
+/* Define to the home page for this package. */
+#undef PACKAGE_URL
+
+/* Define to the version of this package. */
+#undef PACKAGE_VERSION
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
