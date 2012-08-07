cc6d7b67bf1a657a36749fadca390d34a381404e
batrick <batrick@e0a8ed71-7df4-0310-8962-fdc924857419>
2012-08-07 17:37:10 +0000

    Fix r29529. Alias was wrong.

--- nselib/stdnse.lua
+++ nselib/stdnse.lua
@@ -50,7 +50,7 @@ _ENV = require "strict" {};
 -- @class function
 -- @param t Time to sleep, in seconds.
 -- @usage stdnse.sleep(1.5)
-_ENV.sleep = nmap.sleep.socket;
+_ENV.sleep = nmap.socket.sleep;
 
 ---
 -- Prints a formatted debug message if the current debugging level is greater
