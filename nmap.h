db4b8dc6037365837c35ee863902705d6691871d
henri <henri@e0a8ed71-7df4-0310-8962-fdc924857419>
2012-09-03 13:39:43 +0000

    Added an extra level of indirection to let the compiler properly expand the macro. See: http://gcc.gnu.org/onlinedocs/cpp/Stringification.html

--- nmap.h
+++ nmap.h
@@ -256,7 +256,8 @@ void *realloc();
 #define MAX_TIMEOUTS MAX_SOCKETS   /* How many timed out connection attempts 
 				      in a row before we decide the host is 
 				      dead? */
-#define STR(X) #X
+#define _STR(X) #X
+#define STR(X)  _STR(X)
 #define DEFAULT_TCP_PROBE_PORT 80 /* The ports TCP ping probes go to if
                                      unspecified by user -- uber hackers
                                      change this to 113 */
