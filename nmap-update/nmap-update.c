89d52c8f4f44ca50ebfc64a1c2b42dde51fc9012
david <david@e0a8ed71-7df4-0310-8962-fdc924857419>
2012-09-19 15:16:24 +0000

    Declare the function name we actually use.
    It was nmap_update_svn_cmdline_create_auth_baton, but should be
           nmap_update_svn_cmdline_setup_auth_baton.
    This was missed in r27746, which otherwise was making the same change.
    
    This was noticed by John Spencer.
    http://seclists.org/nmap-dev/2012/q3/942

--- nmap-update/nmap-update.c
+++ nmap-update/nmap-update.c
@@ -39,17 +39,16 @@
 
 /* From svn_auth.c. */
 svn_error_t *
-nmap_update_svn_cmdline_create_auth_baton(svn_auth_baton_t **ab,
-                              svn_boolean_t non_interactive,
-                              const char *auth_username,
-                              const char *auth_password,
-                              const char *config_dir,
-                              svn_boolean_t no_auth_cache,
-                              svn_boolean_t trust_server_cert,
-                              svn_config_t *cfg,
-                              svn_cancel_func_t cancel_func,
-                              void *cancel_baton,
-                              apr_pool_t *pool);
+nmap_update_svn_cmdline_setup_auth_baton(svn_auth_baton_t **ab,
+                             svn_boolean_t non_interactive,
+                             const char *auth_username,
+                             const char *auth_password,
+                             const char *config_dir,
+                             svn_boolean_t no_auth_cache,
+                             svn_config_t *cfg,
+                             svn_cancel_func_t cancel_func,
+                             void *cancel_baton,
+                             apr_pool_t *pool);
 
 #include "default_channel.h"
 
