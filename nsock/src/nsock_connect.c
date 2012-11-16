8dd3a8a66eaf528bda9d64e1a51095072b1dee4b
david <david@e0a8ed71-7df4-0310-8962-fdc924857419>
2012-11-16 21:02:54 +0000

    Fix SO_BINDTODEVICE Nsock error message.

--- nsock/src/nsock_connect.c
+++ nsock/src/nsock_connect.c
@@ -108,7 +108,7 @@ static int nsock_make_socket(mspool *ms, msiod *iod, int family, int type, int p
     errno = 0;
     if (setsockopt(iod->sd, SOL_SOCKET, SO_BINDTODEVICE, ms->device, strlen(ms->device) + 1) == -1) {
       if ((errno != EPERM && ms->tracelevel > 0) || ms->tracelevel > 5)
-        nsock_trace(ms, "Setting of SO_BROADCAST failed (IOD #%li)", iod->id);
+        nsock_trace(ms, "Setting of SO_BINDTODEVICE failed (IOD #%li)", iod->id);
     }
 #endif
   }
