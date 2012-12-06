ab802a6f215a2d689968c1286da5db3021ef564b
david <david@e0a8ed71-7df4-0310-8962-fdc924857419>
2012-12-06 02:25:46 +0000

    Update some more headers.

--- nsock/include/nsock_winconfig.h
+++ nsock/include/nsock_winconfig.h
@@ -69,8 +69,8 @@
  *                                                                         *
  * Source code also allows you to port Nmap to new platforms, fix bugs,    *
  * and add new features.  You are highly encouraged to send your changes   *
- * to nmap-dev@insecure.org for possible incorporation into the main       *
- * distribution.  By sending these changes to Fyodor or one of the         *
+ * to the dev@nmap.org mailing list for possible incorporation into the    *
+ * main distribution.  By sending these changes to Fyodor or one of the    *
  * Insecure.Org development mailing lists, it is assumed that you are      *
  * offering the Nmap Project (Insecure.Com LLC) the unlimited,             *
  * non-exclusive right to reuse, modify, and relicense the code.  Nmap     *
