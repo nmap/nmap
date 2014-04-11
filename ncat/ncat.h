7bdd53c868db99cdcd1decf6864c398a8b668e6a
fyodor <fyodor@e0a8ed71-7df4-0310-8962-fdc924857419>
2014-04-11 02:59:07 +0000

    Update Nmap version number from 6.41SVN to 6.45 and rebuild docs

--- ncat/ncat.h
+++ ncat/ncat.h
@@ -146,7 +146,7 @@
 /* Ncat information for output, etc. */
 #define NCAT_NAME "Ncat"
 #define NCAT_URL "http://nmap.org/ncat"
-#define NCAT_VERSION "6.41SVN"
+#define NCAT_VERSION "6.45"
 
 #ifndef __GNUC__
 #ifndef __attribute__
