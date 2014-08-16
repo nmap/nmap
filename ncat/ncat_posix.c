5adfb3b1de162b5fc14b89f5383989af049eb745
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2014-08-16 01:52:33 +0000

    Update copyright notice to 2014

--- ncat/ncat_posix.c
+++ ncat/ncat_posix.c
@@ -2,7 +2,7 @@
  * ncat_posix.c -- POSIX-specific functions.                               *
  ***********************IMPORTANT NMAP LICENSE TERMS************************
  *                                                                         *
- * The Nmap Security Scanner is (C) 1996-2013 Insecure.Com LLC. Nmap is    *
+ * The Nmap Security Scanner is (C) 1996-2014 Insecure.Com LLC. Nmap is    *
  * also a registered trademark of Insecure.Com LLC.  This program is free  *
  * software; you may redistribute and/or modify it under the terms of the  *
  * GNU General Public License as published by the Free Software            *
