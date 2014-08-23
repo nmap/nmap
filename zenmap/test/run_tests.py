b75df9d8f9cb395f75421c656a9b200e9df1ddf9
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2014-08-23 04:22:09 +0000

    Add a test loader script for Zenmap, so tests don't fail with Python <2.7

---
+++ zenmap/test/run_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import unittest
+
+if __name__ == "__main__":
+    import sys
+    import glob
+    import os
+    if not hasattr(unittest.defaultTestLoader, "discover"):
+        print("Python unittest discovery missing. Requires Python 2.7 or newer.")
+        sys.exit(0)
+
+    os.chdir("..")
+    suite = unittest.defaultTestLoader.discover(
+            start_dir=glob.glob("build/lib.*")[0],
+            pattern="*.py"
+            )
+    unittest.TextTestRunner().run(suite)
