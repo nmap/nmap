ce340ba67b2652207906c900437404b2b62e213a
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2014-11-13 13:24:39 +0000

    Fix a typo in Zenmap setup: sys.prefix, not self.prefix

--- zenmap/setup.py
+++ zenmap/setup.py
@@ -235,7 +235,7 @@ class my_install(install):
         # install.finalize_options when sys.prefix is "/usr/local" (our
         # default). Because we need the unchanged value later, remember it
         # here.
-        self.saved_prefix = self.prefix
+        self.saved_prefix = sys.prefix
         install.finalize_options(self)
 
     def run(self):
