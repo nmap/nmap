d33608bb3da43f7d2cc7ec1f58d99b08a55a93f5
Chandan Kumar Singha <cskkman@gmail.com>
2015-03-04 20:49:46 +0530

    semicolon added struct

--- libpcap/pcap-septel.c
+++ libpcap/pcap-septel.c
@@ -55,7 +55,7 @@ static int septel_setnonblock(pcap_t *p, int nonblock, char *errbuf);
  */
 struct pcap_septel {
 	struct pcap_stat stat;
-}
+};
 
 /*
  *  Read at most max_packets from the capture queue and call the callback
