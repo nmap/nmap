8a830a49425f5a1c9e9d47aff99c4c72361611e6
Pierre LALET <pierre.lalet@cea.fr>
2015-04-27 17:38:58 +0200

    NSE: improve ls module output

--- nselib/ls.lua
+++ nselib/ls.lua
@@ -76,7 +76,7 @@ function config(argname)
 end
 
 function new_listing()
-  local output = {}
+  local output = stdnse.output_table()
   output['curvol'] = nil
   output['volumes'] = {}
   output['errors'] = {}
@@ -89,7 +89,7 @@ function new_listing()
 end
 
 function new_vol(output, name, hasperms)
-  local curvol = {}
+  local curvol = stdnse.output_table()
   local files = tab.new()
   local i = 1
   if hasperms then
@@ -105,8 +105,8 @@ function new_vol(output, name, hasperms)
     tab.add(files, i + 3, "CHECKSUM")
   end
   tab.nextrow(files)
-  curvol['files'] = files
   curvol['name'] = name
+  curvol['files'] = files
   curvol['count'] = 0
   curvol['bytes'] = 0
   curvol['errors'] = {}
@@ -145,13 +145,41 @@ local units = {
   ["m"] = 1048576,
   ["g"] = 1073741824,
   ["t"] = 1099511627776,
+  ["p"] = 1125899906842624,
 }
+
+local function get_size(size)
+  local bsize
+  bsize = tonumber(size)
+  if bsize == nil then
+    local unit = string.lower(string.sub(size, -1, -1))
+    bsize = tonumber(string.sub(size, 0, -2))
+    if units[unit] ~= nil and bsize ~= nil then
+      bsize = bsize * units[unit]
+    else
+      bsize = nil
+    end
+  end
+  return bsize
+end
 
 function add_file(output, file)
   -- returns true iff script should continue
   local files = output["curvol"]["files"]
-  local size, bsize
+  local size, isize
+  if output["curvol"]["hasperms"] then
+    isize = 4
+  else
+    isize = 1
+  end
   for i, info in ipairs(file) do
+    if i == isize then
+      size = get_size(info)
+      if size then
+	output["curvol"]["bytes"] = output["curvol"]["bytes"] + size
+	info = size
+      end
+    end
     if type(info) == "number" then
       tab.add(files, i, tostring(info))
     else
@@ -160,22 +188,6 @@ function add_file(output, file)
   end
   tab.nextrow(files)
   output["curvol"]["count"] = output["curvol"]["count"] + 1
-  if output["curvol"]["hasperms"] then
-    size = file[4]
-  else
-    size = file[1]
-  end
-  bsize = tonumber(size)
-  if bsize == nil then
-    local unit = string.lower(string.sub(size, -1, -1))
-    bsize = tonumber(string.sub(size, 0, -2))
-    if units[unit] ~= nil and bsize ~= nil then
-      bsize = bsize * units[unit]
-    else
-      bsize = 0
-    end
-  end
-  output["curvol"]["bytes"] = output["curvol"]["bytes"] + bsize
   return (config("maxfiles") == 0 or config("maxfiles") == nil
 	    or config("maxfiles") > output["curvol"]["count"])
 end
@@ -184,7 +196,7 @@ function end_vol(output)
   local vol = {["volume"] = output["curvol"]["name"]}
   local empty = true
   if #output["curvol"]["files"] ~= 1 then
-    vol["files"] = "\n" .. tab.dump(output["curvol"]["files"])
+    vol["files"] = output["curvol"]["files"]
     empty = false
   end
   if #output["curvol"]["errors"] ~= 0 then
@@ -203,28 +215,105 @@ function end_vol(output)
   output["curvol"] = nil
 end
 
-function end_listing(output)
-  local empty = true
-  if #output["errors"] == 0 then
-    output["errors"] = nil
-  else
-    empty = false
+local function files_to_structured(files)
+  local result = {}
+  local fields = table.remove(files, 1)
+  for i, file in ipairs(files) do
+    result[i] = stdnse.output_table()
+    for j, value in ipairs(file) do
+      result[i][string.lower(fields[j])] = value
+    end
   end
+  return result
+end
+
+local function files_to_readable(files)
+  local outtab = tab.new()
+  local fields = files[1]
+  local file, size, isize, unit, units, outfile
+  tab.addrow(outtab, unpack(fields))
+  for i, field in ipairs(fields) do
+    if string.lower(field) == "size" then
+      isize = i
+      break
+    end
+  end
+  for i = 2, #files do
+    file = files[i]
+    outfile = {}
+    for j, value in ipairs(file) do
+      outfile[j] = value
+    end
+    if config("human") then
+      size = tonumber(outfile[isize])
+      unit = nil
+      units = {"k", "M", "G", "T", "P"}
+      if size ~= nil then
+	while size > 1024 and #units > 0 do
+	  unit = table.remove(units, 1)
+	  size = size / 1024
+	end
+	if unit == nil then
+	  outfile[isize] = tostring(size)
+	else
+	  outfile[isize] = string.format("%.1f %s", size, unit)
+	end
+      end
+    end
+    tab.addrow(outtab, unpack(outfile))
+  end
+  return tab.dump(outtab)
+end
+
+function end_listing(output)
+  assert(output["curvol"] == nil)
+  local line
+  local text = "\n"
+  local empty = true
   if #output["info"] == 0 then
     output["info"] = nil
   else
+    for _, line in ipairs(output["info"]) do
+      text = text .. line .. "\n"
+    end
+    empty = false
+  end
+  if #output["errors"] == 0 then
+    output["errors"] = nil
+  else
+    for _, line in ipairs(output["errors"]) do
+      text = text .. "ERROR: " .. line .. "\n"
+    end
     empty = false
   end
   if #output["volumes"] == 0 then
     output["volumes"] = nil
     output["total"] = nil
   else
+    for _, volume in ipairs(output["volumes"]) do
+      text = text .. "Volume " .. volume["volume"] .. "\n"
+      if volume["info"] then
+	for _, line in ipairs(volume["info"]) do
+	  text = "  " .. text .. line .. "\n"
+	end
+      end
+      if volume["errors"] then
+	for _, line in ipairs(volume["errors"]) do
+	  text = "  " .. text .. "ERROR: " .. line .. "\n"
+	end
+      end
+      if volume["files"] then
+	text = text .. files_to_readable(volume["files"])
+	volume["files"] = files_to_structured(volume["files"])
+      end
+      text = text .. "\n"
+    end
     empty = false
   end
   if empty then
     return nil
   else
-    return output
+    return output, text
   end
 end
 
