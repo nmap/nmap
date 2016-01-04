7300cde3749f831733964106280697197184984c
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2016-01-04 21:44:47 +0000

    Avoid segfault with vasprintf("%s", NULL). http://seclists.org/nmap-dev/2016/q1/4

--- nbase/snprintf.c
+++ nbase/snprintf.c
@@ -242,6 +242,8 @@ append_string (struct state *state,
                int prec,
                int flags)
 {
+  if(!arg)
+    arg = (unsigned char *) "(nall)";
   if(prec != -1)
     width -= prec;
   else
