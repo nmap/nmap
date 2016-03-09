9bcc6c09e22e3a32e8f89a13afee5a9a77b92b62
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2016-03-09 14:52:00 +0000

    Apply patch from Kali package reputed to address #237

--- lpeg.c
+++ lpeg.c
@@ -3340,8 +3340,8 @@ static struct luaL_Reg metareg[] = {
 };
 
 
-int luaopen_lpeg (lua_State *L);
-int luaopen_lpeg (lua_State *L) {
+LUALIB_API int luaopen_lpeg (lua_State *L);
+LUALIB_API int luaopen_lpeg (lua_State *L) {
   luaL_newmetatable(L, PATTERN_T);
   lua_pushnumber(L, MAXBACK);  /* initialize maximum backtracking */
   lua_setfield(L, LUA_REGISTRYINDEX, MAXSTACKIDX);
