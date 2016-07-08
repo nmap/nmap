e41777b72f1835676892ffcd42f8d62d60f0bd57
Sergey Khegay <g.sergeykhegay@gmail.com>
2016-07-08 05:30:09 -0400

    Properly configured build on macos

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 root_dir=$(pwd)
-libssh2_dir=$root_dir/libssh2-1.7.0/
+libssh2_dir=$root_dir/libssh2-1.7.0
 
 echo "Compiling libssh2"
 cd $libssh2_dir
@@ -16,5 +16,17 @@ mkdir BUILD
 ./configure --prefix=$root_dir/BUILD --with-libssh2=$libssh2_dir/BUILD
 make && make install
 echo "Compiled nmap"
+
+
+if [[ "$OSTYPE" == "darwin"* ]]; then 
+	echo "Darwin is detected!";
+	echo "Setting up executable's runtime running path"
+
+	cd $root_dir/BUILD/bin
+	
+	install_name_tool -change $libssh2_dir/BUILD/lib/libssh2.1.dylib \
+							  @loader_path/../../libssh2-1.7.0/BUILD/lib/libssh2.1.dylib \
+							  nmap
+fi
 
 echo "Installation folder: $root_dir/BUILD"
