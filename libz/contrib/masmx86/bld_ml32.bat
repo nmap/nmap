f1cf6f2f2db5a168da9d624d34e7981f518cceab
Sergey Khegay <g.sergeykhegay@gmail.com>
2016-07-18 13:43:37 -0400

    Edit zlib project to build on Windows

--- libz/contrib/masmx86/bld_ml32.bat
+++ libz/contrib/masmx86/bld_ml32.bat
@@ -1,2 +1,2 @@
-ml /coff /Zi /c /Flmatch686.lst match686.asm
-ml /coff /Zi /c /Flinffas32.lst inffas32.asm
+ml /safeseh /coff /Zi /c /Flmatch686.lst match686.asm
+ml /safeseh /coff /Zi /c /Flinffas32.lst inffas32.asm
