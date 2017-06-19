cb17b788b335d8edc1c6801db3f5492295c82c0f
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2017-06-19 22:58:22 +0000

    Clarify indentation

--- ncat/ncat_connect.c
+++ ncat/ncat_connect.c
@@ -1216,8 +1216,8 @@ static void read_stdin_handler(nsock_pool nsp, nsock_event evt, void *data)
 
 
     if (status == NSE_STATUS_EOF) {
-      if (!o.noshutdown)
-        shutdown(nsock_iod_get_sd(cs.sock_nsi), SHUT_WR);
+        if (!o.noshutdown)
+            shutdown(nsock_iod_get_sd(cs.sock_nsi), SHUT_WR);
         /* In --send-only mode or non-TCP mode, exit after EOF on stdin. */
         if (o.proto != IPPROTO_TCP || (o.proto == IPPROTO_TCP && o.sendonly))
             nsock_loop_quit(nsp);
