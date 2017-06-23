bdd67aa68a263413e156510144931b4e2d396142
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2017-06-23 04:44:21 +0000

    Update some error messages to refer to Npcap, not WinPcap.

--- NmapOps.cc
+++ NmapOps.cc
@@ -429,15 +429,11 @@ void NmapOps::ValidateOptions() {
         const char *privreq = "root privileges.";
 #ifdef WIN32
         if (!have_pcap)
-                privreq = "WinPcap version 3.1 or higher and\n\
-iphlpapi.dll.  You seem to be missing one or both of these.  Winpcap is\n\
-available from http://www.winpcap.org.  iphlpapi.dll comes with Win98 and\n\
-later operating systems and NT 4.0 with SP4 or greater.  For previous Windows\n\
-versions, you may be able to take iphlpapi.dll from another system and place\n\
-it in your system32 dir (e.g. c:\\windows\\system32).\n\
-On Windows Vista and Windows 7, The WinPcap NPF service must be started by an\n\
-administrator before WinPcap can be used.  Running nmap.exe will open a UAC\n\
-dialog where you can start NPF if you have administrator privileges.";
+          privreq = "Npcap, but it seems to be missing.\n\
+Npcap is available from http://www.npcap.org. The Npcap driver service must\n\
+be started by an administrator before Npcap can be used. Running nmap.exe\n\
+will open a UAC dialog where you can start the service if you have\n\
+administrator privileges.";
 #endif
 
 
