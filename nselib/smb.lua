c0dc874ff6c065097f4305be7472ef2c8029d33c
calderpwn <paulino@calderonpale.com>
2017-07-08 16:35:38 -0400

    Removes unused SMB2 vars

--- nselib/smb.lua
+++ nselib/smb.lua
@@ -137,7 +137,6 @@ local string = require "string"
 local table = require "table"
 local unicode = require "unicode"
 local smb2 = require "smb2"
-local nsedebug = require "nsedebug"
 _ENV = stdnse.module("smb", stdnse.seeall)
 
 -- These arrays are filled in with constants at the bottom of this file
@@ -3660,27 +3659,7 @@ command_codes =
   SMB_COM_READ_BULK                 = 0xD8,
   SMB_COM_WRITE_BULK                = 0xD9,
   SMB_COM_WRITE_BULK_DATA           = 0xDA,
-  SMB_NO_FURTHER_COMMANDS           = 0xFF,
-  
-  SMB2_COM_NEGOTIATE              = 0x0000,
-  SMB2_COM_SESSION_SETUP          = 0x0001,
-  SMB2_COM_LOGOFF                 = 0x0002,
-  SMB2_COM_TREE_CONNECT           = 0x0003,
-  SMB2_COM_TREE_DISCONNECT        = 0x0004,
-  SMB2_COM_CREATE                 = 0x0005,
-  SMB2_COM_CLOSE                  = 0x0006,
-  SMB2_COM_FLUSH                  = 0x0007,
-  SMB2_COM_READ                   = 0x0008,
-  SMB2_COM_WRITE                  = 0x0009,
-  SMB2_COM_LOCK                   = 0x000A,
-  SMB2_COM_IOCTL                  = 0x000B,
-  SMB2_COM_CANCEL                 = 0x000C,
-  SMB2_COM_ECHO                   = 0x000D,
-  SMB2_COM_QUERY_DIRECTORY        = 0x000E,
-  SMB2_COM_CHANGE_NOTIFY          = 0x000F,
-  SMB2_COM_QUERY_INFO             = 0x0010,
-  SMB2_COM_SET_INFO               = 0x0011,
-  SMB2_COM_OPLOCK_BREAK           = 0x0012
+  SMB_NO_FURTHER_COMMANDS           = 0xFF
 }
 
 for i, v in pairs(command_codes) do
