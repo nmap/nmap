45229e0fbd5172f2d1c2bec84fca533c98a79210
nnposter <nnposter@e0a8ed71-7df4-0310-8962-fdc924857419>
2018-04-26 19:44:58 +0000

    Adds missing --no-shutdown description. Closes #1200

--- ncat/ncat_main.c
+++ ncat/ncat_main.c
@@ -637,6 +637,7 @@ int main(int argc, char *argv[])
 "      --append-output        Append rather than clobber specified output files\n"
 "      --send-only            Only send data, ignoring received; quit on EOF\n"
 "      --recv-only            Only receive data, never send anything\n"
+"      --no-shutdown          Continue half-duplex when receiving EOF on stdin\n"
 "      --allow                Allow only given hosts to connect to Ncat\n"
 "      --allowfile            A file of hosts allowed to connect to Ncat\n"
 "      --deny                 Deny given hosts from connecting to Ncat\n"
