2e08baefd4f9558b74376be7041d3e523cabaec3
nnposter <nnposter@e0a8ed71-7df4-0310-8962-fdc924857419>
2018-07-28 23:06:27 +0000

    Replaces original experimental DNS ECS option code with value prescribed by RFC 7871. Closes #1271

--- nselib/dns.lua
+++ nselib/dns.lua
@@ -1398,7 +1398,7 @@ end
 ---
 -- Adds an client-subnet payload to the OPT packet
 --
--- implementing http://tools.ietf.org/html/draft-vandergaast-edns-client-subnet-00
+-- implementing https://tools.ietf.org/html/rfc7871
 -- @param pkt Table representing DNS packet.
 -- @param Z Table of Z flags. Only DO is supported.
 -- @param client_subnet table containing the following fields
@@ -1407,7 +1407,7 @@ end
 --        <code>address</code> - string containing the IP address
 function addClientSubnet(pkt,Z,subnet)
   local udp_payload_size = 4096
-  local code = 20730 -- temporary option-code http://comments.gmane.org/gmane.ietf.dnsext/19776
+  local code = 8 -- https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-11
   local scope_mask = 0 -- In requests, it MUST be set to 0 see draft
   local data = bin.pack(">SCCA",subnet.family or 1,subnet.mask,scope_mask,ipOps.ip_to_str(subnet.address))
   local opt = bin.pack(">SS",code, #data) .. data
