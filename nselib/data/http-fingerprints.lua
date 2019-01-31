b5b2a5dc78e85fdd2c31667e8b1154e3aa100243
paulino <paulino@e0a8ed71-7df4-0310-8962-fdc924857419>
2019-01-31 21:02:37 +0000

    Adds attack signature for CVE-2018-10824. Closes #1378

--- nselib/data/http-fingerprints.lua
+++ nselib/data/http-fingerprints.lua
@@ -7170,6 +7170,22 @@ table.insert(fingerprints, {
      }
   });
 
+table.insert(fingerprints, {
+    category = 'attacks',
+    probes = {
+      {
+        path = '/uir//tmp/csman/0',
+        method = 'GET'
+      }
+    },
+    matches = {
+      {
+        match = '200',
+        output = 'Possible D-Link router plaintext password file exposure (CVE-2018-10824)'
+      }
+    }
+  });
+
 ------------------------------------------------
 ----        Open Source CMS checks          ----
 ------------------------------------------------
