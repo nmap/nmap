d639a53088cda2d558b32e9176076c4975a7bb4c
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2019-05-28 21:36:04 +0000

    Bump copyright date in license headers

--- ncat/http.h
+++ ncat/http.h
@@ -2,7 +2,7 @@
  * http.h                                                                  *
  ***********************IMPORTANT NMAP LICENSE TERMS************************
  *                                                                         *
- * The Nmap Security Scanner is (C) 1996-2018 Insecure.Com LLC ("The Nmap  *
+ * The Nmap Security Scanner is (C) 1996-2019 Insecure.Com LLC ("The Nmap  *
  * Project"). Nmap is also a registered trademark of the Nmap Project.     *
  * This program is free software; you may redistribute and/or modify it    *
  * under the terms of the GNU General Public License as published by the   *
