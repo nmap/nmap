697a22fd63a4b534ae6658452535fbb9c805898b
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2019-12-30 04:48:50 +0000

    Move some flag defines from services.h to scan_lists.h where they make more sense.

--- services.h
+++ services.h
@@ -142,14 +142,6 @@
 
 #define SERVICE_TABLE_SIZE 1024
 
-/* just flags to indicate whether a particular port number should get tcp
- * scanned, udp scanned, or both
- */
-#define SCAN_TCP_PORT	(1 << 0)
-#define SCAN_UDP_PORT	(1 << 1)
-#define SCAN_SCTP_PORT	(1 << 2)
-#define SCAN_PROTOCOLS	(1 << 3)
-
 int addportsfromservmask(char *mask, u8 *porttbl, int range_type);
 struct servent *nmap_getservbyport(int port, const char *proto);
 void gettoppts(double level, char *portlist, struct scan_lists * ports, char *exclude_list = NULL);
