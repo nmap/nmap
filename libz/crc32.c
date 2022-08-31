0d4036a709ea6948339d38d46d5b61e263e2e8ec
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2022-08-31 20:39:34 +0000

    Fix Visual Studio build of libz on x86 due to type differences

--- libz/crc32.c
+++ libz/crc32.c
@@ -1086,7 +1086,7 @@ uLong ZEXPORT crc32_combine(crc1, crc2, len2)
     uLong crc2;
     z_off_t len2;
 {
-    return crc32_combine64(crc1, crc2, len2);
+    return crc32_combine64(crc1, crc2, (z_off64_t) len2);
 }
 
 /* ========================================================================= */
@@ -1103,11 +1103,11 @@ uLong ZEXPORT crc32_combine_gen64(len2)
 uLong ZEXPORT crc32_combine_gen(len2)
     z_off_t len2;
 {
-    return crc32_combine_gen64(len2);
+    return crc32_combine_gen64((z_off64_t) len2);
 }
 
 /* ========================================================================= */
-uLong crc32_combine_op(crc1, crc2, op)
+uLong ZEXPORT crc32_combine_op(crc1, crc2, op)
     uLong crc1;
     uLong crc2;
     uLong op;
