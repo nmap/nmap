1c63d33f09fd0a3bba725b323a4aa8bdf60c8d7e
Henryk Hruszka <hhruszka@gmail.com>
2022-09-11 22:40:39 +0200

    Fix to support libressl
    Support for libressl was missing causing the following build failure:
    
    nse_ssl_cert.cc:590:53: error: invalid use of incomplete type 'X509' {aka 'struct x509_st'}
      590 |   const char *sig_algo = OBJ_nid2ln(OBJ_obj2nid(cert->sig_alg->algorithm));
          |                                                     ^~
    In file included from /usr/include/openssl/bn.h:133,
                     from nse_ssl_cert.cc:74:
    /usr/include/openssl/ossl_typ.h:147:16: note: forward declaration of 'X509' {aka 'struct x509_st'}
      147 | typedef struct x509_st X509;
          |                ^~~~~~~
    nse_ssl_cert.cc:610:39: error: invalid use of incomplete type 'X509' {aka 'struct x509_st'}
      610 |   if (x509_extensions_to_table(L, cert->cert_info->extensions)) {
          |                                       ^~
    In file included from /usr/include/openssl/bn.h:133,
                     from nse_ssl_cert.cc:74:
    /usr/include/openssl/ossl_typ.h:147:16: note: forward declaration of 'X509' {aka 'struct x509_st'}
      147 | typedef struct x509_st X509;
          |                ^~~~~~~
    nse_ssl_cert.cc:627:35: error: invalid use of incomplete type 'EVP_PKEY' {aka 'struct evp_pkey_st'}
      627 |   pkey_type = EVP_PKEY_type(pubkey->type);
          |                                   ^~
    In file included from /usr/include/openssl/bn.h:133,
                     from nse_ssl_cert.cc:74:
    /usr/include/openssl/ossl_typ.h:121:16: note: forward declaration of 'EVP_PKEY' {aka 'struct evp_pkey_st'}
      121 | typedef struct evp_pkey_st EVP_PKEY;
          |                ^~~~~~~~~~~
    nse_ssl_cert.cc:658:21: error: invalid use of incomplete type 'RSA' {aka 'struct rsa_st'}
      658 |       data->bn = rsa->e;
          |                     ^~
    In file included from /usr/include/openssl/bn.h:133,
                     from nse_ssl_cert.cc:74:
    /usr/include/openssl/ossl_typ.h:138:16: note: forward declaration of 'RSA' {aka 'struct rsa_st'}
      138 | typedef struct rsa_st RSA;
          |                ^~~~~~
    nse_ssl_cert.cc:673:21: error: invalid use of incomplete type 'RSA' {aka 'struct rsa_st'}
      673 |       data->bn = rsa->n;
          |                     ^~
    
    The above error will be present when nmap is build with libressl3.5-libcrypto libressl3.5-libssl libressl3.5-libtls libraries. It will not be present when building with libssl3 (openssl ) library.

--- nse_ssl_cert.cc
+++ nse_ssl_cert.cc
@@ -80,7 +80,8 @@
 #include <openssl/evp.h>
 #include <openssl/err.h>
 
-#if (OPENSSL_VERSION_NUMBER >= 0x10100000L) && !defined LIBRESSL_VERSION_NUMBER
+#if (OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)) || \
+    (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x30500000L)
 /* Technically some of these things were added in 0x10100006
  * but that was pre-release. */
 #define HAVE_OPAQUE_STRUCTS 1
