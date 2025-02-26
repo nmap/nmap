6db5c9cb8577e3c2847c42aca2d95c0b06174110
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2025-02-26 17:44:43 +0000

    Bump copyright date

--- zenmap/zenmapCore/DelayedObject.py
+++ zenmap/zenmapCore/DelayedObject.py
@@ -2,7 +2,7 @@
 
 # ***********************IMPORTANT NMAP LICENSE TERMS************************
 # *
-# * The Nmap Security Scanner is (C) 1996-2024 Nmap Software LLC ("The Nmap
+# * The Nmap Security Scanner is (C) 1996-2025 Nmap Software LLC ("The Nmap
 # * Project"). Nmap is also a registered trademark of the Nmap Project.
 # *
 # * This program is distributed under the terms of the Nmap Public Source
