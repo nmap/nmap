cb5f577848c241f6108b02b93766bfae2149396e
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2025-04-16 05:40:42 +0000

    Remove crashing, unused debug code

--- libnetutil/netutil.cc
+++ libnetutil/netutil.cc
@@ -1604,15 +1604,6 @@ static struct dnet_collector_route_nfo *sysroutes_dnet_find_interfaces(struct dn
   i = 0;
   while (i < dcrn->numroutes) {
     if (dcrn->routes[i].device == NULL) {
-      char destbuf[INET6_ADDRSTRLEN];
-      char gwbuf[INET6_ADDRSTRLEN];
-
-      Strncpy(destbuf, inet_ntop_ez(&dcrn->routes[i].dest, sizeof(dcrn->routes[i].dest)), sizeof(destbuf));
-      Strncpy(gwbuf, inet_ntop_ez(&dcrn->routes[i].gw, sizeof(dcrn->routes[i].gw)), sizeof(gwbuf));
-      /*
-      netutil_error("WARNING: Unable to find appropriate interface for system route to %s/%u gw %s",
-        destbuf, dcrn->routes[i].netmask_bits, gwbuf);
-      */
       /* Remove this entry from the table. */
       memmove(dcrn->routes + i, dcrn->routes + i + 1, sizeof(dcrn->routes[0]) * (dcrn->numroutes - i - 1));
       dcrn->numroutes--;
