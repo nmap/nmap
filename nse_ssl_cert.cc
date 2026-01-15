nmap/nmap

commit 1b3ca45054cd1e569f5fc0da59fceaa7a518b631

Compatibility fixes and Github autobuilds. Closes #3214

nse_ssl_cert.cc
@@ -79,7 +79,8 @@
 #include <openssl/evp.h>
 #include <openssl/err.h>
 
-#if (OPENSSL_VERSION_NUMBER >= 0x10100000L) && !defined LIBRESSL_VERSION_NUMBER
+#if ((OPENSSL_VERSION_NUMBER >= 0x10100000L) && !defined LIBRESSL_VERSION_NUMBER) || \
+    (defined LIBRESSL_VERSION_NUMBER && LIBRESSL_VERSION_NUMBER >= 0x3050000fL)
 /* Technically some of these things were added in 0x10100006
  * but that was pre-release. */
 #define HAVE_OPAQUE_STRUCTS 1
@@ -491,6 +492,22 @@ int lua_push_ecdhparams(lua_State *L, EVP_PKEY *pubkey) {
     /* According to RFC 5480 section 2.1.1, explicit curves must not be used with
        X.509. This may change in the future, but for now it doesn't seem worth it
        to add in code to extract the extra parameters. */
+#if defined(LIBRESSL_VERSION_NUMBER)
+    /* LibreSSL doesn't have EC_GROUP_get_field_type, and explicit curves are rare.
+     * Just mark as UNKNOWN. */
+    lua_pushstring(L, "UNKNOWN");
+#elif HAVE_OPAQUE_STRUCTS
+    nid = EC_GROUP_get_field_type(group);
+    if (nid == NID_X9_62_prime_field) {
+      lua_pushstring(L, "explicit_prime");
+    }
+    else if (nid == NID_X9_62_characteristic_two_field) {
+      lua_pushstring(L, "explicit_char2");
+    }
+    else {
+      lua_pushstring(L, "UNKNOWN");
+    }
+#else
     nid = EC_METHOD_get_field_type(EC_GROUP_method_of(group));
     if (nid == NID_X9_62_prime_field) {
       lua_pushstring(L, "explicit_prime");
@@ -499,9 +516,9 @@ int lua_push_ecdhparams(lua_State *L, EVP_PKEY *pubkey) {
       lua_pushstring(L, "explicit_char2");
     }
     else {
-      /* Something weird happened. */
       lua_pushstring(L, "UNKNOWN");
     }
+#endif
     lua_setfield(L, -2, "ec_curve_type");
   }
   lua_setfield(L, -2, "curve_params");

.github/workflows/build.yml
@@ -1,0 +1,206 @@
+name: nmap multiplatform autobuilds
+
+on:
+  push:
+    paths:
+      - ".github/workflows/build.yml"
+      - "**/*.c"
+      - "**/*.cc"
+      - "**/*.cpp"
+      - "**/*.h"
+      - "**/*.H"
+      - "**/*.in"
+      - "**/*.am"
+      - "**/*.ac"
+      - "**/*.lua"
+      - "**/*.cmakein"
+      - "**/configure"
+      - "**/*.pl"
+      - "**/*.py"
+      - "**/*.awk"
+      - "**/*.sh"
+      - "**/*.toml"
+    branches:
+      - master
+  pull_request:
+  workflow_dispatch:
+    inputs:
+      deploymentName:
+        description: "Name for this deployment"
+        required: true
+        default: "Manual Deployment"
+
+concurrency:
+  group: ${{ github.workflow }}-${{ github.ref }}
+  cancel-in-progress: true
+
+env:
+  MAKEFLAGS: -j3
+
+jobs:
+  build:
+    strategy:
+      fail-fast: false
+      matrix:
+        include:
+          - name: ubuntu-latest-gcc
+            os: ubuntu-latest
+            cc: gcc
+          - name: ubuntu-latest-gcc-arm64
+            os: ubuntu-latest
+            cc: gcc
+            arch: arm64
+          - name: ubuntu-latest-clang
+            os: ubuntu-latest
+            cc: clang
+          - name: macos-15-clang
+            os: macos-15
+            cc: clang
+          - name: macos-26-clang
+            os: macos-26
+            cc: clang
+          - name: freebsd-15-gcc
+            os: ubuntu-latest
+            cc: gcc
+          - name: freebsd-15-clang
+            os: ubuntu-latest
+            cc: clang
+          - name: openbsd-7-gcc
+            os: ubuntu-latest
+            cc: egcc
+          - name: openbsd-7-clang
+            os: ubuntu-latest
+            cc: clang
+          - name: netbsd-10-gcc
+            os: ubuntu-latest
+            cc: gcc
+          - name: netbsd-10-clang
+            os: ubuntu-latest
+            cc: clang
+          - name: solaris-11-gcc
+            os: ubuntu-latest
+            cc: gcc
+          - name: solaris-11-clang
+            os: ubuntu-latest
+            cc: clang
+          - name: windows-latest-msvc
+            os: windows-latest
+            cc: msvc
+
+    runs-on: ${{ matrix.os }}
+    steps:
+      - uses: actions/checkout@v4
+
+      - name: Set up QEMU for ARM64
+        if: matrix.arch == 'arm64'
+        uses: docker/setup-qemu-action@v3
+        with:
+          platforms: linux/arm64
+
+      - name: Build Linux
+        if: startsWith(matrix.name,'ubuntu') && matrix.arch != 'arm64'
+        run: |
+          sudo apt-get update
+          sudo apt-get install -y build-essential clang tree libpcap-dev libnet-dev libpcre2-dev
+          ./configure
+          make
+          make install DESTDIR=/tmp
+          tree /tmp/usr
+
+      - name: Build Linux ARM64
+        if: startsWith(matrix.name,'ubuntu') && matrix.arch == 'arm64'
+        run: |
+          docker run --rm --platform linux/arm64 -v $PWD:/work -w /work ubuntu:latest bash -c "
+            apt-get update -q -y &&
+            apt-get install -q -y build-essential tree libpcap-dev libnet-dev libpcre2-dev autoconf automake &&
+            ./configure
+            make &&
+            make install DESTDIR=/tmp &&
+            tree /tmp/usr
+          "
+
+      - name: Build MacOS
+        if: startsWith(matrix.name,'macos')
+        run: |
+          brew update
+          # PCRE2 is already installed
+          brew install tree libpcap libnet
+          ./configure
+          make
+          make install DESTDIR=/tmp
+          tree /tmp/usr
+
+      - name: Build FreeBSD
+        if: startsWith(matrix.name,'freebsd')
+        uses: vmactions/freebsd-vm@v1
+        with:
+          release: "15.0"
+          usesh: true
+          prepare: |
+            # OS has libpcap already installed (and adding puts a second in /usr/local)
+            pkg install -y gcc llvm autotools tree pkgconf libnet pcre2 gmake dbus
+          run: |
+            ./configure
+            gmake
+            gmake install DESTDIR=/tmp
+            tree /tmp/usr
+
+      - name: Build OpenBSD
+        if: startsWith(matrix.name,'openbsd')
+        uses: vmactions/openbsd-vm@v1
+        with:
+          usesh: true
+          prepare: |
+            export PKG_PATH=https://cdn.openbsd.org/pub/OpenBSD/$(uname -r)/packages/$(uname -m)/
+            pkg_add -I autoconf%2.72 automake%1.17 gcc%11 llvm%19 tree pkgconf libnet%1.1 pcre2 gmake dbus
+          run: |
+            export AUTOCONF_VERSION=2.72
+            export AUTOMAKE_VERSION=1.17
+            ./configure
+            gmake
+            gmake install DESTDIR=/tmp
+            tree /tmp/usr
+
+      - name: Build NetBSD
+        if: startsWith(matrix.name,'netbsd')
+        uses: vmactions/netbsd-vm@v1
+        with:
+          usesh: true
+          prepare: |
+            export PATH=/usr/sbin:/usr/pkg/sbin:/usr/pkg/bin:$PATH
+            export PKG_PATH="http://cdn.netbsd.org/pub/pkgsrc/packages/NetBSD/$(uname -p)/$(uname -r | cut -d_ -f1)/All"
+            pkg_add gcc13 clang autoconf automake pkgconf libpcap libnet gmake dbus
+          run: |
+            ./configure
+            gmake
+            gmake install DESTDIR=/tmp
+            tree /tmp/usr
+
+      - name: Build Solaris
+        if: startsWith(matrix.name,'solaris')
+        uses: vmactions/solaris-vm@v1
+        with:
+          usesh: true
+          prepare: |
+            pkg install gcc-c clang autoconf automake pcre2 libpcap libnet developer/build/gnu-make
+          run: |
+            ./configure
+            gmake
+            gmake install DESTDIR=/tmp
+            tree /tmp/usr
+
+      - name: Install Subversion
+        if: startsWith(matrix.name,'windows')
+        shell: powershell
+        run: |
+          choco install svn -y --no-progress
+          $env:PATH = "C:\Program Files (x86)\Subversion\bin;C:\Program Files\Subversion\bin;$env:PATH"
+          echo "C:\Program Files (x86)\Subversion\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+          echo "C:\Program Files\Subversion\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+
+      - name: Build Windows
+        if: startsWith(matrix.name,'windows')
+        shell: cmd
+        run: |
+          cd mswin32
+          Build.bat

Makefile.in
@@ -312,7 +312,7 @@ distclean-%: clean-%
 	-cd $* && $(MAKE) distclean
 
 install-nmap: $(TARGET)
-	$(INSTALL) -d $(DESTDIR)$(bindir) $(DESTDIR)$(mandir)/man1 $(DESTDIR)$(nmapdatadir)
+	mkdir -p $(DESTDIR)$(bindir) $(DESTDIR)$(mandir)/man1 $(DESTDIR)$(nmapdatadir)
 	$(INSTALL) -c -m 755 nmap $(DESTDIR)$(bindir)/nmap
 # Use strip -x to avoid stripping dynamically loaded NSE functions. See
 # http://seclists.org/nmap-dev/2007/q4/0272.html.
@@ -320,7 +320,7 @@ install-nmap: $(TARGET)
 	$(INSTALL) -c -m 644 docs/$(TARGET).1 $(DESTDIR)$(mandir)/man1/
 	if [ "$(USE_NLS)" = "yes" ]; then \
 	  for ll in $(filter $(ALL_LINGUAS),$(LINGUAS)); do \
-	    $(INSTALL) -d $(DESTDIR)$(mandir)/$$ll/man1; \
+	    mkdir -p $(DESTDIR)$(mandir)/$$ll/man1; \
 	    $(INSTALL) -c -m 644 docs/man-xlate/$(TARGET)-$$ll.1 $(DESTDIR)$(mandir)/$$ll/man1/$(TARGET).1; \
 	  done; \
 	fi
@@ -354,7 +354,7 @@ build-zenmap: $(ZENMAPDIR)/pyproject.toml $(ZENMAPDIR)/zenmapCore/Version.py
 	$(PYTHON) -m build $(ZENMAPDIR)/
 
 install-zenmap: $(ZENMAPDIR)/pyproject.toml
-	$(INSTALL) -d $(DESTDIR)$(bindir) $(DESTDIR)$(mandir)/man1 $(DESTDIR)$(deskdir)
+	mkdir -p $(DESTDIR)$(bindir) $(DESTDIR)$(mandir)/man1 $(DESTDIR)$(deskdir)
 	$(PYTHON) -m pip install $(ZENMAPDIR)/ $(if $(DESTDIR),--root "$(DESTDIR)")
 	$(INSTALL) -c -m 644 docs/zenmap.1 $(DESTDIR)$(mandir)/man1/
 	$(INSTALL) -c -m 644 $(ZENMAPDIR)/install_scripts/unix/*.desktop $(DESTDIR)$(deskdir)
@@ -373,7 +373,7 @@ build-nping: $(NPINGDIR)/Makefile build-nbase build-nsock build-netutil $(NPINGD
 	@cd $(NPINGDIR) && $(MAKE)
 
 install-ndiff:
-	$(INSTALL) -d $(DESTDIR)$(bindir) $(DESTDIR)$(mandir)/man1
+	mkdir -p $(DESTDIR)$(bindir) $(DESTDIR)$(mandir)/man1
 	$(PYTHON) -m pip install $(NDIFFDIR)/ $(if $(DESTDIR),--root "$(DESTDIR)")
 	$(INSTALL) -c -m 644 $(NDIFFDIR)/docs/ndiff.1 $(DESTDIR)$(mandir)/man1/
 
@@ -381,8 +381,8 @@ NSE_FILES = scripts/script.db scripts/*.nse
 NSE_LIB_LUA_FILES = nselib/*.lua nselib/*.luadoc
 
 install-nse: $(TARGET)
-	$(INSTALL) -d $(DESTDIR)$(nmapdatadir)/scripts
-	$(INSTALL) -d $(DESTDIR)$(nmapdatadir)/nselib
+	mkdir -p $(DESTDIR)$(nmapdatadir)/scripts
+	mkdir -p $(DESTDIR)$(nmapdatadir)/nselib
 
 # Remove obsolete scripts from a previous installation.
 	(cd $(DESTDIR)$(nmapdatadir)/scripts && rm -f $(OLD_SCRIPT_NAMES))
@@ -390,9 +390,9 @@ install-nse: $(TARGET)
 	$(INSTALL) -c -m 644 nse_main.lua $(DESTDIR)$(nmapdatadir)/
 	$(INSTALL) -c -m 644 $(NSE_FILES) $(DESTDIR)$(nmapdatadir)/scripts
 	$(INSTALL) -c -m 644 $(NSE_LIB_LUA_FILES) $(DESTDIR)$(nmapdatadir)/nselib
-	$(INSTALL) -d $(DESTDIR)$(nmapdatadir)/nselib/data
+	mkdir -p $(DESTDIR)$(nmapdatadir)/nselib/data
 	for f in `find nselib/data -name .svn -prune -o -type d -print`; do \
-		$(INSTALL) -d $(DESTDIR)$(nmapdatadir)/$$f; \
+		mkdir -p $(DESTDIR)$(nmapdatadir)/$$f; \
 	done
 	for f in `find nselib/data -name .svn -prune -o -type f -print`; do \
 		$(INSTALL) -c -m 644 $$f $(DESTDIR)$(nmapdatadir)/$$f; \

charpool.cc
@@ -64,6 +64,7 @@
 #include <stddef.h>
 #undef NDEBUG
 #include <assert.h>
+#include <climits>
 
 #include "nbase.h"
 

configure
@@ -7258,6 +7258,7 @@ fi
 fi
 
 if test $have_libpcap != yes; then
+  ac_configure_args="$ac_configure_args '--disable-dbus'"
   subdirs="$subdirs libpcap"
 
   if test "${LIBPCAP_INC+set}" = "set"; then
@@ -7274,6 +7275,48 @@ printf "%s\n" "#define PCAP_INCLUDED 1" >>confdefs.h
 
 
 printf "%s\n" "#define HAVE_PCAP_SET_IMMEDIATE_MODE 1" >>confdefs.h
+
+  # Check if D-Bus is available (in case --disable-dbus didn't work)
+  # and add it to LIBS if needed for static linking
+  { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking for dbus_connection_read_write in -ldbus-1" >&5
+printf %s "checking for dbus_connection_read_write in -ldbus-1... " >&6; }
+if test ${ac_cv_lib_dbus_1_dbus_connection_read_write+y}
+then :
+  printf %s "(cached) " >&6
+else $as_nop
+  ac_check_lib_save_LIBS=$LIBS
+LIBS="-ldbus-1  $LIBS"
+cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+/* Override any GCC internal prototype to avoid an error.
+   Use char because int might match the return type of a GCC
+   builtin and then its argument prototype would still apply.  */
+char dbus_connection_read_write ();
+int
+main (void)
+{
+return dbus_connection_read_write ();
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_link "$LINENO"
+then :
+  ac_cv_lib_dbus_1_dbus_connection_read_write=yes
+else $as_nop
+  ac_cv_lib_dbus_1_dbus_connection_read_write=no
+fi
+rm -f core conftest.err conftest.$ac_objext conftest.beam \
+    conftest$ac_exeext conftest.$ac_ext
+LIBS=$ac_check_lib_save_LIBS
+fi
+{ printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: $ac_cv_lib_dbus_1_dbus_connection_read_write" >&5
+printf "%s\n" "$ac_cv_lib_dbus_1_dbus_connection_read_write" >&6; }
+if test "x$ac_cv_lib_dbus_1_dbus_connection_read_write" = xyes
+then :
+  LIBS="$LIBS -ldbus-1"
+fi
 
 else
   # We assume our included libpcap doesn't need this check-and-define
@@ -7997,8 +8040,6 @@ ac_link='$CC -o conftest$ac_exeext $CFLAGS $CPPFLAGS $LDFLAGS conftest.$ac_ext $
 ac_compiler_gnu=$ac_cv_c_compiler_gnu
 
     # We need Lua 5.4 exactly
-    { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking for lua version == 504" >&5
-printf %s "checking for lua version == 504... " >&6; }
     ac_ext=c
 ac_cpp='$CPP $CPPFLAGS'
 ac_compile='$CC -c $CFLAGS $CPPFLAGS conftest.$ac_ext >&5'
@@ -8165,6 +8206,10 @@ else $as_nop
   have_lua=no
 fi
 rm -f conftest.err conftest.i conftest.$ac_ext
+    { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking for lua version == 504" >&5
+printf %s "checking for lua version == 504... " >&6; }
+    { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: $have_lua" >&5
+printf "%s\n" "$have_lua" >&6; }
     ac_ext=c
 ac_cpp='$CPP $CPPFLAGS'
 ac_compile='$CC -c $CFLAGS $CPPFLAGS conftest.$ac_ext >&5'
@@ -8178,9 +8223,11 @@ ac_compiler_gnu=$ac_cv_c_compiler_gnu
   fi
 
   # if we didn't find we use our own
+  { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking whether lua was found" >&5
+printf %s "checking whether lua was found... " >&6; }
   if test $have_lua != yes; then
-    { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: no" >&5
-printf "%s\n" "no" >&6; }
+    { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: no, will use our own" >&5
+printf "%s\n" "no, will use our own" >&6; }
     CPPFLAGS="-I\$(top_srcdir)/$LIBLUADIR $CPPFLAGS"
     LIBLUA_LIBS="\$(top_srcdir)/$LIBLUADIR/liblua.a"
     LUA_BUILD="build-lua"
@@ -8298,9 +8345,11 @@ done
 fi
 
 # if we didn't find we use our own
+{ printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking whether liblinear was found" >&5
+printf %s "checking whether liblinear was found... " >&6; }
 if test $have_liblinear != yes; then
-  { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: no" >&5
-printf "%s\n" "no" >&6; }
+  { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: no, will use our own" >&5
+printf "%s\n" "no, will use our own" >&6; }
   CPPFLAGS="-I\$(top_srcdir)/$LIBLINEARDIR $CPPFLAGS"
   LIBLINEAR_LIBS="\$(top_srcdir)/$LIBLINEARDIR/liblinear.a"
   LIBLINEAR_BUILD="build-liblinear"

configure.ac
@@ -495,6 +495,9 @@ if test $have_libpcap != yes; then
   PCAP_DIST_CLEAN="distclean-pcap"
   AC_DEFINE(PCAP_INCLUDED, 1, [Using included libpcap])
   AC_DEFINE(HAVE_PCAP_SET_IMMEDIATE_MODE, 1, [Included libpcap has pcap_set_immediate_mode])
+  # Check if D-Bus is available (in case --disable-dbus didn't work)
+  # and add it to LIBS if needed for static linking
+  AC_CHECK_LIB([dbus-1], [dbus_connection_read_write], [LIBS="$LIBS -ldbus-1"])
 else
   # We assume our included libpcap doesn't need this check-and-define
   # link with -lpcap for the purposes of this test

libdnet-stripped/src/arp-bsd.c
@@ -35,6 +35,11 @@
 
 #include "dnet.h"
 
+/* NetBSD 10+ removed RTF_LLINFO */
+#ifndef RTF_LLINFO
+#define RTF_LLINFO 0
+#endif
+
 struct arp_handle {
 	int	fd;
 	int	seq;
@@ -138,13 +143,13 @@ arp_add(arp_t *arp, const struct arp_entry *entry)
 		errno = EADDRNOTAVAIL;
 		return (-1);
 	}
-	if (sin->sin_addr.s_addr == entry->arp_pa.addr_ip) {
-		if ((msg.rtm.rtm_flags & RTF_LLINFO) == 0 ||
-		    (msg.rtm.rtm_flags & RTF_GATEWAY) != 0) {
-			errno = EADDRINUSE;
-			return (-1);
-		}
-	}
+    if (sin->sin_addr.s_addr == entry->arp_pa.addr_ip) {
+        if ((RTF_LLINFO && ((msg.rtm.rtm_flags & RTF_LLINFO) == 0)) ||
+            (msg.rtm.rtm_flags & RTF_GATEWAY) != 0) {
+            errno = EADDRINUSE;
+            return (-1);
+        }
+    }
 	if (sa->sa_family != AF_LINK) {
 		errno = EADDRNOTAVAIL;
 		return (-1);

libdnet-stripped/src/intf.c
@@ -904,7 +904,7 @@ intf_get_src(intf_t *intf, struct intf_entry *entry, struct addr *src)
 int
 intf_get_dst(intf_t *intf, struct intf_entry *entry, struct addr *dst)
 {
-	union sockunion sun;
+	union sockunion su;
 	socklen_t n;
 
 	int fd;
@@ -913,19 +913,19 @@ intf_get_dst(intf_t *intf, struct intf_entry *entry, struct addr *dst)
 		errno = EINVAL;
 		return (-1);
 	}
-	addr_ntos(dst, (struct sockaddr *)&sun);
-	sun.sin.sin_port = htons(666);
+	addr_ntos(dst, (struct sockaddr *)&su);
+	su.sin.sin_port = htons(666);
 
 	fd = dst->addr_type == ADDR_TYPE_IP6 ? intf->fd6 : intf->fd;
-	if (connect(fd, (struct sockaddr *)&sun, sizeof(sun)) < 0)
+	if (connect(fd, (struct sockaddr *)&su, sizeof(su)) < 0)
 		return (-1);
-	
-	n = sizeof(sun);
-	if (getsockname(fd, (struct sockaddr *)&sun, &n) < 0)
+
+	n = sizeof(su);
+	if (getsockname(fd, (struct sockaddr *)&su, &n) < 0)
 		return (-1);
-	
-	addr_ston((struct sockaddr *)&sun, &entry->intf_addr);
-	
+
+	addr_ston((struct sockaddr *)&su, &entry->intf_addr);
+
 	if (intf_loop(intf, _match_intf_src, entry) != 1)
 		return (-1);
 	

commit c30cd046a66a6181cbf01851a7049506060457f8

Fix EC_METHOD compilation error with OpenSSL 1.1.x
The function EC_GROUP_get_field_type() does not exist in OpenSSL 1.1.x.
This commit uses EC_METHOD_get_field_type(EC_GROUP_method_of(group))
which is the correct method for OpenSSL 1.1.x and was used before
commit 1b3ca45.

Fixes compilation error introduced in commit 1b3ca45054cd1e569f5fc0da59fceaa7a518b631.

nse_ssl_cert.cc
@@ -497,7 +497,7 @@ int lua_push_ecdhparams(lua_State *L, EVP_PKEY *pubkey) {
      * Just mark as UNKNOWN. */
     lua_pushstring(L, "UNKNOWN");
 #elif HAVE_OPAQUE_STRUCTS
-    nid = EC_GROUP_get_field_type(group);
+    nid = EC_METHOD_get_field_type(EC_GROUP_method_of(group));
     if (nid == NID_X9_62_prime_field) {
       lua_pushstring(L, "explicit_prime");
     }