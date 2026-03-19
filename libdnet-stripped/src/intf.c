8cf08ee9cb8bee9aeb8068709e632482f69433ba
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2026-03-19 17:39:59 +0000

    Handle EAFNOSUPPORT as on FreeBSD

--- libdnet-stripped/src/intf.c
+++ libdnet-stripped/src/intf.c
@@ -177,9 +177,14 @@ intf_open(void)
 
 #if defined(SIOCGLIFCONF) || defined(SIOCGIFNETMASK_IN6) || defined(SIOCGIFNETMASK6)
 		if ((intf->fd6 = socket(AF_INET6, SOCK_DGRAM, 0)) < 0) {
+			if (1
 #  ifdef EPROTONOSUPPORT
-			if (errno != EPROTONOSUPPORT)
+					&& errno != EPROTONOSUPPORT
 #endif
+#  ifdef EAFNOSUPPORT
+					&& errno != EAFNOSUPPORT
+#endif
+					)
 				return (intf_close(intf));
 		}
 #endif
