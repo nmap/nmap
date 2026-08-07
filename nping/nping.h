542eda7cf64edcf33e4e49c9f7952b12a97946db
dmiller <dmiller@e0a8ed71-7df4-0310-8962-fdc924857419>
2026-08-07 15:40:04 +0000

    Bump version, use git

--- nping/nping.h
+++ nping/nping.h
@@ -265,7 +265,7 @@
 /* General tunable defines  **************************************************/
 #define NPING_NAME "Nping"
 #define NPING_URL "https://nmap.org/nping"
-#define NPING_VERSION "7.99SVN"
+#define NPING_VERSION "7.991"
 
 
 #define DEFAULT_VERBOSITY VB_0
