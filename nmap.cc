4c36cf12f246b52a8d510bdde8becd5c5b3bf8b5
nnposter <nnposter@e0a8ed71-7df4-0310-8962-fdc924857419>
2026-08-12 19:29:05 +0000

    Do not call NSE if compiling without Lua support. Close #3434

--- nmap.cc
+++ nmap.cc
@@ -1858,7 +1858,9 @@ void nmap_free_mem() {
   AllProbes::service_scan_free();
   traceroute_hop_cache_clear();
   nsock_set_default_engine(NULL);
+#ifndef NOLUA
   close_nse();
+#endif
 }
 
 int nmap_main(int argc, char *argv[]) {
